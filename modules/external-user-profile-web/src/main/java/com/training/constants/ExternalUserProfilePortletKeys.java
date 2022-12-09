653439761d50a42b555794b38a81461ab2e7eb8f
aecioprado <aecioprado@gmail.com>
2022-12-09 11:07:19 -0300

    Create module external-user-profile-web

---
+++ modules/external-user-profile-web/src/main/java/com/training/constants/ExternalUserProfilePortletKeys.java
@@ -0,0 +1,11 @@
+package com.training.constants;
+
+/**
+ * @author me
+ */
+public class ExternalUserProfilePortletKeys {
+
+	public static final String EXTERNALUSERPROFILE =
+		"com_training_ExternalUserProfilePortlet";
+
+}
